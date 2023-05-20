80a8ded7a21a6caa4d89b91932b42146183113a7
georgi-stoilov <gdstoilov@gmail.com>
2023-05-20 23:18:18 +0200

    commit2

--- core.sol
+++ core.sol
@@ -1,36 +1,89 @@
-function calculateDelay(address _user, uint256 _amount) internal view returns (uint256) {
-    uint256 baseDelay = 2 hours; // default delay
-    uint256 trustScore = getUserTrustScore(_user); // get the trust score of the user
+pragma solidity ^0.8.0;
 
-    // Decrease delay based on trust score
-    uint256 trustScoreDelay = baseDelay * trustScore / 100; 
+contract MyContract {
+    function calculateDelay(
+        address _user,
+        uint256 _amount
+    ) internal view returns (uint256) {
+        uint256 baseDelay = 2 hours; // default delay
+        uint256 trustScore = getUserTrustScore(_user); // get the trust score of the user
 
-    // Increase delay based on transaction size (e.g., every 1000 tokens increase delay by 10%)
-    uint256 sizeDelay = baseDelay * _amount / 1000 * 10 / 100; 
+        // Decrease delay based on trust score
+        uint256 trustScoreDelay = (baseDelay * trustScore) / 100;
 
-    return baseDelay - trustScoreDelay + sizeDelay;
+        // Increase delay based on transaction size (e.g., every 1000 tokens increase delay by 10%)
+        uint256 sizeDelay = (((baseDelay * _amount) / 1000) * 10) / 100;
+
+        return baseDelay - trustScoreDelay + sizeDelay;
+    }
+
+    // transaction details struct
+    struct Transaction {
+        address user;
+        uint256 amount;
+        uint256 executionTime;
+        uint256 userScore;
+    }
+
+    // pending transactions mapping
+    mapping(uint256 => Transaction) public pendingTransactions;
+
+    // transaction processing function
+    function processTransaction(uint256 _transactionId) public {
+        require(
+            block.timestamp >=
+                pendingTransactions[_transactionId].executionTime,
+            "Transaction delay period not over"
+        );
+
+        // call the function to execute the transaction
+        executeTransaction(
+            pendingTransactions[_transactionId].user,
+            pendingTransactions[_transactionId].amount
+        );
+
+        // delete the transaction from the pending list
+        delete pendingTransactions[_transactionId];
+    }
+
+    function getUserTrustScore(address _user) internal pure returns (uint256) {
+        // Implement your logic to retrieve the trust score of the user
+        // This function should return the trust score as a uint256 value
+        // You can replace this with your own implementation or connect to an external data source
+        // For demonstration purposes, we will simply return a hardcoded trust score of 80
+        return 80;
+    }
+
+    function getUserTrustScore(address _user) public view returns (uint256) {
+        uint256 score = 100; // default score
+        // adjust the score based on transaction history, frequency and size
+        // transactionHistory and transactionFrequency are hypothetical mappings that store user data on-chain
+        if (transactionHistory[_user].length > 100) {
+            score += 10;
+        }
+        if (transactionFrequency[_user] > 10) {
+            score += 10;
+        }
+        // For simplicity, we're directly adding/subtracting scores based on conditions.
+        // In practice, these calculations could be more complex.
+        return score;
+    }
+
+    function getUserTrustScore(address _user) public view returns (uint256) {
+        uint256 score = 100; // default score
+
+        // Adjust the score based on transaction history, frequency, and size
+        // Assume transactionHistory and transactionFrequency are mappings that store user data on-chain
+        if (transactionHistory[_user].length > 100) {
+            score += 10;
+        }
+        if (transactionFrequency[_user] > 10) {
+            score += 10;
+        }
+
+        // For simplicity, we're directly adding/subtracting scores based on conditions.
+        // In practice, these calculations could be more complex.
+
+        return score;
+    }
 }
-
-
-
-// transaction details struct
-struct Transaction {
-    address user;
-    uint256 amount;
-    uint256 executionTime;
-}
-
-// pending transactions mapping
-mapping (uint256 => Transaction) public pendingTransactions;
-
-// transaction processing function
-function processTransaction(uint256 _transactionId) public {
-    require(block.timestamp >= pendingTransactions[_transactionId].executionTime, "Transaction delay period not over");
-
-    // call the function to execute the transaction
-    executeTransaction(pendingTransactions[_transactionId].user, pendingTransactions[_transactionId].amount);
-
-    // delete the transaction from the pending list
-    delete pendingTransactions[_transactionId];
-}
-
